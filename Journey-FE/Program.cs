274471ce4716eed93ef4236b19818f150826d0bf
stoyan stoyanov - stucky <stoyan.v.stoyanov86@gmail.com>
2023-04-04 14:22:49 +0300

    Add project files.

---
+++ Journey-FE/Program.cs
@@ -0,0 +1,11 @@
+using Journey_FE;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7135/api") });
+
+await builder.Build().RunAsync();
